8f8ffb8519879d6dacfd4870781b32b19cc75e2f
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-05-02 17:50:06 +0200

    Middleware 'auth' in routes

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::prefix('admin')->group(function () {
+Route::middleware('auth')->prefix('admin')->group(function () {
 
     Route::get('/home', 'HomeController@index')->name('home');
 });
