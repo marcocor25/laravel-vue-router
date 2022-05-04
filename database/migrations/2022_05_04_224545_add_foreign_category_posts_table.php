f84e25c69359defa3bd7bd4d0483ea00f4f5a409
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-05-05 01:07:37 +0200

    category_id migration completed

--- database/migrations/2022_05_04_224545_add_foreign_category_posts_table.php
+++ database/migrations/2022_05_04_224545_add_foreign_category_posts_table.php
@@ -14,7 +14,13 @@ class AddForeignCategoryPostsTable extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+
+            $table->unsignedBigInteger('category_id')->nullable()->after('slug');
+
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories')
+                ->onDelete('set null');
         });
     }
 
@@ -26,7 +32,10 @@ class AddForeignCategoryPostsTable extends Migration
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+
+            $table->dropForeign(['category_id']);
+
+            $table->dropColumn('category_id');
         });
     }
 }
