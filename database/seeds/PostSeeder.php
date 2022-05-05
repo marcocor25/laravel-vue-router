c2ea0e5d66248f122bca30803b57de5d1db02514
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-05-05 16:37:07 +0200

    Added random categories on post seeder

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Category;
 use App\Post;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
@@ -15,11 +16,16 @@ class PostSeeder extends Seeder
     public function run(Faker $faker)
     {
 
+        $categories = Category::all();
+
+        $categoriesId = $categories->pluck('id')->all();
+
         for ($i = 0; $i < 50; $i++) {
 
             $post = new Post();
             $post->title = $faker->words(8, true);
             $post->slug = Str::slug($post->title);
+            $post->category_id = $faker->optional()->randomElement($categoriesId);
             $post->content = $faker->paragraph(5, true);
             $post->image = $faker->url();
             $post->published_at = $faker->optional()->dateTime();
