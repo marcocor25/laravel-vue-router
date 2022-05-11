fe77bd8d68bd362f33705204bee753f85a4342d1
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-05-12 01:16:04 +0200

    npm run watch

--- public/js/front.js
+++ public/js/front.js
@@ -15904,7 +15904,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/marcocorallo/Dev/LARAVEL/laravel-api/resources/js/front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! /Users/marcocorallo/Dev/LARAVEL/laravel-vue-router/resources/js/front.js */"./resources/js/front.js");
 
 
 /***/ })
