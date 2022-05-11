ab97e170cf7e5ff9f15e3be56920a96f55e457da
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-05-11 16:40:15 +0200

    Imported Nunito Sans

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -11,8 +11,7 @@
     <title>Posts @yield('metaTitle')</title>
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/front.css') }}" rel="stylesheet">
